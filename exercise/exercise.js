68206e76b409f87f28134551731d5c46977f8ced
Miriana Falcone <miriana.falcone7@gmail.com>
2023-09-08 10:56:58 +0200

    promise

---
+++ exercise/exercise.js
@@ -0,0 +1,20 @@
+const isLogged = true;
+let promiseOne = new Promise((resolve, reject) => {
+    if (isLogged) {
+        resolve(Math.random());
+    }
+    reject("error");
+});
+
+let promiseTwo = (number) =>
+    new Promise((resolve, reject) => {
+        if (number > 0, 5) {
+            resolve({ name: "John", age: 24 });
+        }
+        reject("The number isn't greater than 0,5");
+    });
+
+promiseOne
+    .then(promiseTwo)
+    .then((result) => console.log(result))
+    .then((err) => console.error(err));
